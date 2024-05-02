2b2b9fef9cf8c78a7d530240b710141bb4d121fa
Kinjal Jain <jainkinjal021196@gmail.com>
2024-05-02 13:17:54 +0530

    modified font size

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -36,12 +36,12 @@ return (
 
                     <div className="mt-5">
                       <h3 className="text-white font-bold text-[24px]">{name}</h3>
-                      <p className="mt-2 text-secondary text-[14px]">{description}</p>
+                      <p className="mt-2 text-secondary text-[12px]">{description}</p>
                     </div>
 
                     <div className="mt-4 flex flex-wrap gap-2">
                       {tags.map((tag)=> (
-                        <p key={tag.name} className={`text-[14px] ${tag.color}`}>
+                        <p key={tag.name} className={`text-[10px] ${tag.color}`}>
                           #{tag.name}
                         </p>
                       ))}
